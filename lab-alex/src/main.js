be5cbbe2b54857a9e26b9c6dbd7912029244d6b5
warlordlizard <alexjbowers88@gmail.com>
2018-03-21 14:25:47 -0700

    compiles basic app with style

---
+++ lab-alex/src/main.js
@@ -0,0 +1,24 @@
+'use strict';
+
+import './style/main.scss';
+import React from 'react';
+import ReactDom from 'react-dom';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      notes: [],
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <p>stuff goes here</p>
+      </div>
+    )
+  }
+}
+
+ReactDom.render(<App />, document.getElementById('root'));
