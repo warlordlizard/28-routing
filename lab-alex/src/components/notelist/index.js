e4c77bc2ef1ed3c9f33ab2a7de6d4e653552ec4c
warlordlizard <alexjbowers88@gmail.com>
2018-03-24 18:28:52 -0700

    prteey much fits wireframe

--- lab-alex/src/components/notelist/index.js
+++ lab-alex/src/components/notelist/index.js
@@ -1,3 +1,35 @@
 'use strict';
 
 import React from 'react';
+import NoteForm from '../noteform';
+import './_notelist.scss';
+
+class NoteList extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return(
+      <section className='note-list'>
+        <ul>
+          {this.props.notes.map((item) => 
+            <li key={item.id}>
+              <button  className='remove' onClick={() => this.props.removeNote(item)}>X</button>
+              <p>- {item.content}</p>
+              <NoteForm 
+                note={item}
+                submitTitle='update' 
+                handleSubmit={(note) => {
+                  note.id = item.id;
+                  this.props.updateNote(note);
+                }} />
+                
+            </li>
+          )}
+        </ul>
+      </section>
+    );
+  }
+}
+
+export default NoteList;
